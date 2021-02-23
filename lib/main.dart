c8e1f2696980d231b0d44966b0fbbc0203fba3d0
Bismark de Carvalho <47608746+Bismark-Carvalho@users.noreply.github.com>
2021-02-22 22:23:46 -0300

    Alinhamento widget DROPDOWNBUTTON tela principal.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       initialRoute: '/',
       routes: {
--- lib/src/models/views/HomeScreen.dart
+++ lib/src/models/views/HomeScreen.dart
@@ -6,27 +6,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 100),
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(1000),
-                    child: Image.network(
-                        "https://media.istockphoto.com/vectors/currency-converter-base-icon-simple-sign-illustration-currency-can-vector-id1157097828")),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 100),
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(1000),
+                      child: Image.network(
+                          "https://media.istockphoto.com/vectors/currency-converter-base-icon-simple-sign-illustration-currency-can-vector-id1157097828")),
+                ),
               ),
-            ),
-            TesteWidGet(),
-            TesteWidGet(),
-            TesteWidGet(),
-          ],
+              TesteWidGet(),
+              TesteWidGet(),
+              TesteWidGet(),
+              TesteWidGet(),
+              BtnConverter()
+            ],
+          ),
         ),
       ),
     );
@@ -39,41 +44,83 @@ class TesteWidGet extends StatefulWidget {
 }
 
 class _TesteWidGetState extends State<TesteWidGet> {
-
   String value = "Euro";
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 40.0, bottom: 0),
+      padding: const EdgeInsets.only(right: 40.0, bottom: 10),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          DropdownButton(
-              value: value,
-              icon: Icon(Icons.arrow_downward),
-              iconSize: 15,
-
-
-              items: <String>["Real", "Dolar", "Euro", "Bitcoin"]
-                  .map<DropdownMenuItem<String>>((e) {
-                return DropdownMenuItem<String>(
-                  value: e,
-                  child: Text(e),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  value = newValue;
-                });
-              }),
-          Container(
-            width: 225,
-            child: TextField(
-              keyboardType: TextInputType.number,
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 57,
+                child: DropdownButton(
+                    isExpanded: true,
+                    underline: Container(
+                      height: 1,
+                      color: Colors.purple,
+                    ),
+                    hint: Text("Bismark"),
+                    value: value,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 15,
+                    items: <String>["Real", "Dolar", "Euro", "Bitcoin"]
+                        .map<DropdownMenuItem<String>>((e) {
+                      return DropdownMenuItem<String>(
+                        value: e,
+                        child: Text(e),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        value = newValue;
+                      });
+                    }),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.amber))),
+              ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BtnConverter extends StatefulWidget {
+  @override
+  _BtnConverterState createState() => _BtnConverterState();
+}
+
+class _BtnConverterState extends State<BtnConverter> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: RaisedButton(
+        child: Text("Converter"),
+        color: Colors.purple,
+        textColor: Colors.white,
+        onPressed: () {
+          // TODO implementar metodo de converter
+          print("");
+        },
       ),
     );
   }
